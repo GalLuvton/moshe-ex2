6efdd8fe1129b766912d288cdbe54c94d7c3c407
GalLuvton <galluvton@gmail.com>
2016-05-25 19:49:55 +0300

    now the file doesn't look like crap

--- Part2.py
+++ Part2.py
@@ -8,24 +8,55 @@ import numpy as np
 from collections import defaultdict
 from Part1 import *
 
-q = 0
-p = 0
 k = 20
 maxt = 1
 epsilon = 0.01
-varray = {}
-uarray = {}
-bbook = {}
-st = {}
+st = 0
+sv = 0
+uarray = 0
+varray = 0
+bbook = 0
+
+
+class DataRatingVector(object):
+    def __init__(self, size):
+        self.len = size
+        self.data = [0 for _ in range(size)]
+        self.mapping = defaultdict(set)
+
+    def clean(self):
+        self.data = [0 for _ in range(self.len)]
+        self.mapping = defaultdict(set)
+
+    def randomize(self):
+        tmp = [random.randint(0, k - 1) for _ in range(self.len)]
+        for itm in range(self.len):  # 'itm' is the data id, 'tmp[itm]' is the cluster id
+            self.data[itm] = tmp[itm]
+            self.mapping[tmp[itm]] = itm
+
+    def add_data(self, cluster, id_):
+        self.data[id_] = cluster
+        self.mapping[cluster].add(id_)
+
+    def get_by_cluster(self, cluster):
+        return self.mapping[cluster]
+
+    def get_by_id(self, id_):
+        return self.data[id_]
 
 
 def recalcbbook():
-    tmpbbook = np.empty(shape=(k, k))
-    tmpbbook.fill(0)
+    global uarray
+    global varray
+    global bbook
+    global st
+
+    bbook = np.empty(shape=(k, k))
+    bbook.fill(0)
     for bi in range(k):
         for bj in range(k):
-            usr_set = uarray[bi]
-            itm_set = varray[bj]
+            usr_set = uarray.get_by_cluster(bi)
+            itm_set = varray.get_by_cluster(bj)
             isum = 0
             icount = 0
             for usr in usr_set:
@@ -34,87 +65,56 @@ def recalcbbook():
                     if rating > 0:
                         isum += rating
                         icount += 1
-            if icount != 0:
-                tmpbbook[(bi, bj)] = isum / icount
-    return tmpbbook
+            if icount == 0:
+                bbook[(bi, bj)] = st.avg_rating
+            else:
+                bbook[(bi, bj)] = isum / icount
 
 
 def rmse():
-    totalerror = 0
-    usrcount = 0
-    new_uarray = defaultdict(set)
-    for _, cluster in uarray.iteritems():
-        for usr in cluster:
-            user_ratings = st.get_user_by_id(usr+1)
-            errormin = 99999
-            cur_user_cluster = 0
-            for ti in range(k):
-                tcurerror = 0
-                flagset = 0
-                for itmkey, value in user_ratings._attr_rating_pairs():
-                    itm_cluster = 0
-                    for clst, tcluster in varray.iteritems():
-                        if itmkey in tcluster:
-                            itm_cluster = clst
-                            break
-                    a = st.get_rating_by_uid_iid(value + 1, itmkey + 1)
-                    b = bbook[(ti, itm_cluster)]
-                    if a > 0 and b > 0:
-                        flagset = 1
-                        tcurerror += (a - b) ** 2
-                if flagset and tcurerror < errormin:
-                    errormin = tcurerror
-                    cur_user_cluster = ti
-            new_uarray[cur_user_cluster].add(usr)
-            totalerror += errormin
-            usrcount += 1
-    global uarray
-    uarray = new_uarray
-    return totalerror / usrcount
+    global bbook
+    global sv
+
+    #  iterate over sv - for every i,j in sv, find cluster_of_i and cluster_of_j
+    #  calculate (bbook[cluster_of_i, cluster_of_j - sv[i,j])**2
+    #  sum into total_error, return (total_error / num_of_items)
+
+    total_error = 0
+    count = 0
+
+    return total_error / count
+
+
+def recalc_vector(vec1, vec2):
+    global bbook
+    global st
+    global k
+
+    vec1.clean()
+    for i in range(vec1.len):
+        newarr = [0 for _ in range(k)]
+        for arj in range(k):
+            for j in range(vec2.len):
+                usrs_cluster = vec2.get_by_id(j)
+                a = st.get_rating_by_uid_iid(i + 1, j + 1)
+                b = bbook[(arj, usrs_cluster)]
+                newarr[arj] += (a - b) ** 2
+        new_cluster = np.argmin(newarr)
+        vec1.add_data(new_cluster, i)
 
 
 def recalc_ratings():
-    global bbook
+    global uarray
+    global varray
+
     t = 1
     lasterror = 99999
-    bbook = recalcbbook()
+    recalcbbook()
     while t <= maxt:
-        new_uarray = defaultdict(set)
-        for i in range(p):
-            newarr = [0 for _ in range(k)]
-            for arj in range(k):
-                for j in range(q):
-                    usrs_cluster = 0
-                    for clst, cluster in varray.iteritems():
-                        if j in cluster:
-                            usrs_cluster = clst
-                            break
-                    a = st.get_rating_by_uid_iid(i + 1, j + 1)
-                    b = bbook[(arj, usrs_cluster)]
-                    newarr[arj] += (a - b) ** 2
-            new_cluster = np.argmin(newarr)
-            new_uarray[new_cluster].add(i)
-        global uarray
-        uarray = new_uarray
-        bbook = recalcbbook()
-        new_varray = defaultdict(set)
-        for j in range(q):
-            newarr = [0 for _ in range(k)]
-            for arj in range(k):
-                for i in range(p):
-                    usrs_cluster = 0
-                    for clst, cluster in uarray.iteritems():
-                        if i in cluster:
-                            usrs_cluster = clst
-                            break
-                    a = st.get_rating_by_uid_iid(i + 1, j + 1)
-                    b = bbook[(usrs_cluster, arj)]
-                    newarr[arj] += (a - b) ** 2
-            new_cluster = np.argmin(newarr)
-            new_varray[new_cluster].add(j)
-        global varray
-        varray = new_varray
-        bbook = recalcbbook()
+        recalc_vector(uarray, varray)
+        recalcbbook()
+        recalc_vector(varray, uarray)
+        recalcbbook()
         curerror = rmse()
         t += 1
         if lasterror * (1 - epsilon) < curerror:
@@ -129,17 +129,20 @@ def write_to_csv(profiles, filename):
             writer.writerow(row)
 
 
-def write_set_to_csv(vector, len, filename):
-    flat_arr = [0 for _ in range(len)]
-    for clst, cluster in vector.iteritems():
-        for eid in cluster:
-            flat_arr[eid] = clst
+def write_dataratingvector_to_csv(vector, filename):
     with open(filename, 'wb') as f:
         writer = csv.writer(f)
-        writer.writerow(flat_arr)
+        writer.writerow(vector.data)
 
 
 def main():
+    global k
+    global maxt
+    global epsilon
+    global varray
+    global uarray
+    global st
+
     if not (len(sys.argv) == 6 or len(sys.argv) == 9) or sys.argv[1] != 'ExtractCB':
         print ('usage: python Part2.py ExtractCB '
                '[the rating input file] [[K size] [T size] [ε size]] [U output directory as csv file] '
@@ -152,39 +155,24 @@ def main():
     if len(sys.argv) == 6:
         input_file, u_output_dir, v_output_dir, b_output_dir = sys.argv[2:]
     if len(sys.argv) == 9:
-        global k
-        global maxt
-        global epsilon
         input_file, sk, smaxt, sepsilon, u_output_dir, v_output_dir, b_output_dir = sys.argv[2:]
         k = int(sk)
         maxt = int(smaxt)
         epsilon = float(sepsilon)
 
-    global st
     st = Profiles.create(input_file)
-
-    global q
-    q = len(st._items)
-    global p
-    p = len(st._users)
-    global varray
-    tmparray = [random.randint(0, k - 1) for _ in range(q)]
-    varray = defaultdict(set)
-    for i in range(q):
-        varray[tmparray[i]].add(i)  # 'i' being userId, tmparray[i] being its cluster
-    global uarray
-    tmparray = [random.randint(0, k - 1) for _ in range(q)]
-    uarray = defaultdict(set)
-    for i in range(q):
-        uarray[tmparray[i]].add(i)  # 'i' being itemId, tmparray[i] being its cluster
+    varray = DataRatingVector(len(st._items))
+    varray.randomize()
+    uarray = DataRatingVector(len(st._users))
+    uarray.randomize()
 
     recalc_ratings()
 
     print "Writing U vector ..."
-    write_set_to_csv(uarray, p, u_output_dir+"UVector.csv")
+    write_dataratingvector_to_csv(uarray, u_output_dir+"UVector.csv")
     print 'DONE'
     print "Writing V vector ..."
-    write_set_to_csv(varray, q, v_output_dir+"VVector.csv")
+    write_dataratingvector_to_csv(varray, v_output_dir+"VVector.csv")
     print 'DONE'
     print "Writing B matrix ..."
     write_to_csv(bbook, b_output_dir+"BMatrix.csv")
